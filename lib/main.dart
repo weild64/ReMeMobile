b07cf0dc52436ed87ae89146e3fd1131029a328a
weild64 <80021065+weild64@users.noreply.github.com>
2021-03-06 10:59:59 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+const warteg_url = 'https://s.id/wartegepel';
 
 void main(){
   runApp(new MaterialApp(
@@ -11,7 +13,9 @@ void main(){
       '/Halepel' : (BuildContext context) => new Halepel(),
       '/Hallily' : (BuildContext context) => new Hallily(),
       '/Halrey' : (BuildContext context) => new Halrey(),
-      '/Halchloe' : (BuildContext context) => new Halchloe()
+      '/Halchloe' : (BuildContext context) => new Halchloe(),
+      '/Halwarteg' : (BuildContext context) => new Halwarteg(),
+      '/Halsettings' : (BuildContext context) => new Halsettings(),
     }
   ));
 }
@@ -57,9 +61,15 @@ class Halawal extends StatelessWidget {
                 },
               ),//button chloe
               FlatButton(
-                child: new ButtonPlacement(icon: Icons.add_a_photo, teks:"Settings"),
+                child: new ButtonWarteg(/*icon: Icons.add_a_photo, teks:"All About Chloe pawapua"*/),
                 onPressed: (){
-                  Navigator.pushNamed(context, '/Halepel');
+                  Navigator.pushNamed(context, '/Halwarteg');
+                },
+              ),
+              FlatButton(
+                child: new ButtonPlacement(icon: Icons.settings, teks:"Settings"),
+                onPressed: (){
+                  Navigator.pushNamed(context, '/Halsettings');
                 },
               ),//button setting
       ],
@@ -86,9 +96,11 @@ class Halepel extends StatelessWidget {
           ),// Profile
           ElevatedButton(
             child: Text("Donation"),
-            onPressed: () {
-              Navigator.pushNamed(context, '/Halawal');
-            },
+              onPressed: () async {
+                if (await canLaunch('https://trakteer.id/evelynvtuber')){
+                  await launch('https://trakteer.id/evelynvtuber');
+                };
+              }
           ), //Donation
           ElevatedButton(
             child: Text("Schedule"),
@@ -126,9 +138,11 @@ class Hallily extends StatelessWidget {
             ),// Profile
             ElevatedButton(
               child: Text("Donation"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/Halawal');
-              },
+                onPressed: () async {
+                  if (await canLaunch('https://trakteer.id/lilyifeta')){
+                    await launch('https://trakteer.id/lilyifeta');
+                  };
+                }
             ), //Donation
             ElevatedButton(
               child: Text("Schedule"),
@@ -166,9 +180,11 @@ class Halrey extends StatelessWidget {
             ),// Profile
             ElevatedButton(
               child: Text("Donation"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/Halawal');
-              },
+                onPressed: () async {
+                  if (await canLaunch('https://trakteer.id/reynardblanc')){
+                    await launch('https://trakteer.id/reynardblanc');
+                  };
+                }
             ), //Donation
             ElevatedButton(
               child: Text("Schedule"),
@@ -206,9 +222,11 @@ class Halchloe extends StatelessWidget {
             ),// Profile
             ElevatedButton(
               child: Text("Donation"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/Halawal');
-              },
+                onPressed: () async {
+                  if (await canLaunch('https://trakteer.id/chloepawapua')){
+                    await launch('https://trakteer.id/chloepawapua');
+                  };
+                }
             ), //Donation
             ElevatedButton(
               child: Text("Schedule"),
@@ -228,6 +246,109 @@ class Halchloe extends StatelessWidget {
     );
   }
 }
+
+class Halwarteg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Warteg Epelz Page"),),
+      body: new Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text("Jadwal Podcast"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ),// Profile
+            ElevatedButton(
+              child: Text("Jadwal Radio"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ), //Donation
+            ElevatedButton(
+              child: Text("Jadwal Makrab"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ),// sched
+            ElevatedButton(
+              child: Text("Discord Link"),
+              onPressed: () async {
+                if (await canLaunch('https://s.id/wartegepel')){
+                  await launch('https://s.id/wartegepel');
+                };
+              }
+              ),
+            ElevatedButton(
+              child: Text("Donation"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ),
+            ElevatedButton(
+              child: Text("Kembali"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ), //Kembali
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Halsettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Settings"),),
+      body: new Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start ,
+          children: <Widget>[
+            FlatButton(
+              child: Text("Setting Alarm"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ),// Profile
+            FlatButton(
+                child: Text("About Us"),
+              onPressed: (){
+                  showAboutDialog(context: context,
+                  applicationIcon: FlutterLogo(),
+                    applicationName: 'Warteg Epel Project',
+                    applicationVersion: '0.0.2',
+                    applicationLegalese: 'Dibuat Oleh DuoSimpTeam',
+                    children: <Widget> [
+                      Text('Tentang Kami Duo Simp, Kami Terdiri dari Dua orang Berinisialkan Renkyushi dan Zafkiel'),
+                ],
+                  );
+              },
+            ), //Donation
+            FlatButton(
+              child: Text("Schedule"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ),// sched
+            FlatButton(
+              child: Text("Kembali"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Halawal');
+              },
+            ), //Kembali
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class ButtonPlacement extends StatelessWidget {
   ButtonPlacement({this.icon, this.teks, this.navigasi});
@@ -338,6 +459,27 @@ class ButtonChloe extends StatelessWidget{
         child: new Column(
           children: <Widget>[
             new Image.asset('Image/BannerChloe.png')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonWarteg extends StatelessWidget{
+  ButtonWarteg({this.navigasi});
+
+  final String navigasi;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(0.0),
+      color: Colors.blueAccent,
+      child: new Card(
+        child: new Column(
+          children: <Widget>[
+            new Image.asset('Image/BannerWarteg.png')
           ],
         ),
       ),
